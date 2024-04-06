a6cdeeecd73a7b3bd93c27182ce6ce822816c25a
Zach Orosz <zach.orosz@gmail.com>
2024-04-06 11:08:22 -0400

    customize alabaster colors

--- lua/zachorosz/plugins/colors.lua
+++ lua/zachorosz/plugins/colors.lua
@@ -1,10 +1,18 @@
+local function colorscheme(color)
+	color = color or "alabaster"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
 return {
 	{
 		"p00f/alabaster.nvim",
 		priority = 1000,
 		config = function()
 			vim.g.alabaster_dim_comments = true
-			vim.cmd.colorscheme("alabaster")
+			colorscheme("alabaster")
 		end,
 	},
 }
