8508b0100ddfe247888944e224eab3b95a722334
Zach Orosz <zach.orosz@gmail.com>
2024-10-04 20:07:37 -0400

    custom colors

--- lua/zachorosz/plugins/colors.lua
+++ lua/zachorosz/plugins/colors.lua
@@ -1,18 +1,39 @@
-local function colorscheme(color)
-	color = color or "alabaster"
-	vim.cmd.colorscheme(color)
+local function color_overrides()
+	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Type", { link = "Normal" })
+	vim.api.nvim_set_hl(0, "Keyword", { link = "Normal" })
+end
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+local function disable_italics()
+	local hl_groups = vim.api.nvim_get_hl(0, {})
+
+	for key, hl_group in pairs(hl_groups) do
+		if hl_group.italic then
+			vim.api.nvim_set_hl(0, key, vim.tbl_extend("force", hl_group, { italic = false }))
+		end
+	end
 end
 
 return {
+	--{
+	--	"p00f/alabaster.nvim",
+	--	priority = 1000,
+	--	config = function()
+	--		vim.g.alabaster_dim_comments = true
+	--		vim.g.alabaster_floatborder = true
+	--		vim.cmd.colorscheme("alabaster")
+	--		color_overrides()
+	--	end,
+	--},
 	{
-		"p00f/alabaster.nvim",
+		"kvrohit/rasmus.nvim",
 		priority = 1000,
 		config = function()
-			vim.g.alabaster_dim_comments = true
-			colorscheme("alabaster")
+			vim.g.rasmus_italic_comments = false
+			vim.cmd.colorscheme("rasmus")
+			color_overrides()
+			disable_italics()
 		end,
 	},
 }
